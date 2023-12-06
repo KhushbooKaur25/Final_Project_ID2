cb6f8cbb1e7f35257ea9bcdc1567515358762636
Khush <143151015+KhushbooKaur25@users.noreply.github.com>
2023-12-05 20:41:31 -0800

    Update khushboo.js

--- khushboo.js
+++ khushboo.js
@@ -1,45 +1,42 @@
-//draw a spinning octahedron
-let octahedron;
-let octahedron2;
-let logoScale = 0.01;
+let sketch4 = (p) => {
+  let octahedron;
+  let octahedron2;
+  let logoScale = 0.01;
 
-function preload() {
-  octahedron = loadModel('bluecube3.obj');
-  octahedron2 = loadModel('pinkcube3.obj');
-}
+  p.preload = function () {
+    octahedron = p.loadModel("bluecube3.obj");
+    octahedron2 = p.loadModel("pinkcube3.obj");
+  };
 
-function setup() {
-  createCanvas(400,400, WEBGL);
-  // describe('Vertically rotating 3-d octahedron.');
-}
+  p.setup = function () {
+    let canvas = p.createCanvas(400, 400, p.WEBGL);
+    canvas.parent("sketch4Container");
+  };
 
-function draw() {
- background(200);
-  
-  ambientLight(250);
-  // Set the color of the 3D model to DC6399
-  ambientMaterial(220,99,153);
-  pointLight(100,20, 255);
-  
-  push();
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.02);
-  scale(8,8,8);
-  model(octahedron);
-  pop();
-  
-  
-  
- // ambientLight(250);
-  // Set the color of the 3D model to DC6399
-  ambientMaterial(118,195,230);
- pointLight(21,20, 25);
-  
-  
-  push();
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  scale(8,8,8);
-  model(octahedron2);
-  pop();
-}
+  p.draw = function () {
+    p.background(200);
+
+    p.ambientLight(250);
+    p.ambientMaterial(220, 99, 153);
+    p.pointLight(100, 20, 255);
+
+    p.push();
+    p.rotateX(p.frameCount * 0.01);
+    p.rotateY(p.frameCount * 0.02);
+    p.scale(8, 8, 8);
+    p.model(octahedron);
+    p.pop();
+
+    p.ambientMaterial(118, 195, 230);
+    p.pointLight(21, 20, 25);
+
+    p.push();
+    p.rotateX(p.frameCount * 0.01);
+    p.rotateY(p.frameCount * 0.01);
+    p.scale(8, 8, 8);
+    p.model(octahedron2);
+    p.pop();
+  };
+};
+
+new p5(sketch4);
